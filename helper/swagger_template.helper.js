401970eff06b5a8cd7bfb1dd7c5920c0e5ebf291
wahyupambudi <wahyupambudi823@gmail.com>
2023-11-19 20:42:27 +0700

    add basic project

---
+++ helper/swagger_template.helper.js
@@ -0,0 +1,22 @@
+const swaggerDefinition = {
+    swaggerDefinition: {
+        info: {
+            title: 'Testing FGA Issue',
+            version: '1.0.0',
+            description: 'Your API description',
+        },
+        servers: [
+            {
+                url: 'http://localhost:8080',
+            },
+            {
+                url: 'http://localhost:3000',
+            },
+
+        ],
+    },
+    apis: ['./routes/user.route.js'],
+
+}
+
+module.exports = swaggerDefinition
