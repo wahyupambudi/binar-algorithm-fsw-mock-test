c1bdb294f8cb35e62084c3bab0a4ea62c7bd6a97
wahyupambudi <wahyupambudi823@gmail.com>
2023-11-20 14:14:27 +0700

    add prisma, controller, routes

---
+++ controller/todo.controller.js
@@ -0,0 +1,10 @@
+const { ResponseTemplate } = require("../helper/template.helper");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function Insert(req, res) {
+
+}
+
+module.exports = {Insert}
