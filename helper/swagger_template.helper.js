5ccd1946c80280e38625f1bb5184592ed3549f87
wahyupambudi <wahyupambudi823@gmail.com>
2023-11-21 17:04:34 +0700

    Update swagger_template.helper.js

--- helper/swagger_template.helper.js
+++ helper/swagger_template.helper.js
@@ -12,7 +12,9 @@ const swaggerDefinition = {
             {
                 url: 'http://localhost:3000',
             },
-
+            {
+                url: 'https://binar-algorithm-fsw-mock-test-production.up.railway.app",
+            },
         ],
     },
     apis: ['./routes/user.route.js'],
